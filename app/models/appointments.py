2ac200e1197fbb719b81afaf40f3662567852cba
Liam Wilson <liamw5265@gmail.com>
2026-09-10 10:11:31 -0700

    Fixed foreign key columns and relationship func

--- app/models/appointments.py
+++ app/models/appointments.py
@@ -1,11 +1,10 @@
-from app.database import Base
+from model_base import Base
 
-from sqlalchemy import String, Integer, DATETIME, DATE, TIME
-from sqlalchemy.dialects.mysql import SMALLINT, BIGINT
+from sqlalchemy import String, DATETIME, DATE, TIME, ForeignKey
+from sqlalchemy.dialects.mysql import SMALLINT, INTEGER, BIGINT
 from sqlalchemy.orm import mapped_column, relationship
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from datetime import datetime, timezone
 
 class Appointment(Base):
     __tablename__ = 'appointments'
@@ -18,17 +17,20 @@ class Appointment(Base):
     )
 
     parient_id = mapped_column(
-        Integer(unsigned = True),
+        INTEGER(unsigned = True),
+        ForeignKey('patients.parient_id'),
         nullable = False
     )
 
     provider_id = mapped_column(
         SMALLINT(unsigned = True),
+        ForeignKey('providers.provider_id'),
         nullable = True
     )
 
     department_id = mapped_column(
         SMALLINT(unsigned = False),
+        ForeignKey('departments.department_id'),
         nullable = False
     )
 
@@ -64,27 +66,27 @@ class Appointment(Base):
     created_at = mapped_column(
         DATETIME,
         nullable = False,
-        default = datetime.now(ZoneInfo('US/Hawaii')).strftime('%Y-%m-%d %H:%M:%S')
+        default = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     )
 
     updated_at = mapped_column(
         DATETIME,
         nullable = False,
-        default = datetime.now(ZoneInfo('US/Hawaii')).strftime('%Y-%m-%d %H:%M:%S'),
-        onupdate = datetime.now(ZoneInfo('US/Hawaii')).strftime('%Y-%m-%d %H:%M:%S')
+        default = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
+        onupdate = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     )
 
     patients = relationship(
-        'appointments',
-        back_populates = 'patients'
+        'Patient',
+        back_populates = 'appointments'
     )
 
     providers = relationship(
-        'appointments',
-        back_populates = 'providers'
+        'Provider',
+        back_populates = 'appointments'
     )
 
     departments = relationship(
-        'appointments',
-        back_populates = 'departments'
+        'Department',
+        back_populates = 'appointments'
     )
