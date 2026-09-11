e2ef85319763dd2dbfa3f5124489938d689c1d3b
Liam Wilson <liamw5265@gmail.com>
2026-09-10 18:47:15 -0700

    Added relationships and fixed parameters

--- app/models/appointments.py
+++ app/models/appointments.py
@@ -1,35 +1,34 @@
-from model_base import Base
+from app.models.model_base import Base
 
-from sqlalchemy import String, DATETIME, DATE, TIME, ForeignKey
-from sqlalchemy.dialects.mysql import SMALLINT, INTEGER, BIGINT
+from sqlalchemy import ForeignKey, DATE, TIME, String, DATETIME, text, func
+from sqlalchemy.dialects.mysql import BIGINT, INTEGER, SMALLINT
 from sqlalchemy.orm import mapped_column, relationship
 
-from datetime import datetime, timezone
 
 class Appointment(Base):
     __tablename__ = 'appointments'
 
-    appointment = mapped_column(
+    appointment_id = mapped_column(
         BIGINT(unsigned = True),
         autoincrement = True,
         nullable = False,
         primary_key = True
     )
 
-    parient_id = mapped_column(
+    patient_id = mapped_column(
         INTEGER(unsigned = True),
-        ForeignKey('patients.parient_id'),
+        ForeignKey('patients.patient_id'),
         nullable = False
     )
 
     provider_id = mapped_column(
         SMALLINT(unsigned = True),
         ForeignKey('providers.provider_id'),
-        nullable = True
+        nullable = False
     )
 
     department_id = mapped_column(
-        SMALLINT(unsigned = False),
+        SMALLINT(unsigned = True),
         ForeignKey('departments.department_id'),
         nullable = False
     )
@@ -66,14 +65,14 @@ class Appointment(Base):
     created_at = mapped_column(
         DATETIME,
         nullable = False,
-        default = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        server_default = text('CURRENT_TIMESTAMP')
     )
 
     updated_at = mapped_column(
         DATETIME,
         nullable = False,
-        default = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S'),
-        onupdate = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        server_default = text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        onupdate = func.current_timestamp()
     )
 
     patients = relationship(
@@ -88,5 +87,10 @@ class Appointment(Base):
 
     departments = relationship(
         'Department',
+        back_populates = 'appointments'
+    )
+
+    encounters = relationship(
+        'Encounter',
         back_populates = 'appointments'
     )
