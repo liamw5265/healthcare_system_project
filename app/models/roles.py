e1f65250021e6e4278700bb79a17dd66c2042aa4
Liam Wilson <liamw5265@gmail.com>
2026-09-10 18:49:23 -0700

    Added relationships and fixed parameters

--- app/models/roles.py
+++ app/models/roles.py
@@ -1,6 +1,7 @@
-from app.database import Base
+from app.models.model_base import Base
 
-from sqlalchemy import String, Integer
+from sqlalchemy import String
+from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.orm import mapped_column, relationship
 
 
@@ -8,7 +9,7 @@ class Role(Base):
     __tablename__ = 'roles'
 
     role_id = mapped_column(
-        Integer(unsigned = True),
+        INTEGER(unsigned = True),
         autoincrement = True,
         nullable = False,
         primary_key = True
@@ -16,9 +17,15 @@ class Role(Base):
 
     role_name = mapped_column(
         String(100),
-        nullable = False
+        nullable = False,
+        unique = True
     )
 
     description = mapped_column(
         String(255)
+    )
+
+    user_roles = relationship(
+        'UserRole',
+        back_populates = 'roles'
     )
