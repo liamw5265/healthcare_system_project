0f5361226965afaa2b463351d37e41a4b202a54e
Liam Wilson <liamw5265@gmail.com>
2026-09-10 18:49:41 -0700

    Added relationships and fixed parameters

--- app/models/user_roles.py
+++ app/models/user_roles.py
@@ -1,20 +1,23 @@
-from app.database import Base
+from app.models.model_base import Base
 
-from sqlalchemy import Integer
-from sqlalchemy.dialects.mysql import BIGINT
+from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.mysql import BIGINT, INTEGER
 from sqlalchemy.orm import mapped_column, relationship
+
 
 class UserRole(Base):
     __tablename__ = 'user_roles'
 
     user_id = mapped_column(
         BIGINT(unsigned = True),
+        ForeignKey('users.user_id'),
         nullable = False,
         primary_key = True
     )
 
     role_id = mapped_column(
-        Integer(unsigned = True),
+        INTEGER(unsigned = True),
+        ForeignKey('roles.role_id'),
         nullable = False,
         primary_key = True
     )
