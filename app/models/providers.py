c27e094ce2411ef5883ce1576b9e2f86b42ac76a
Liam Wilson <liamw5265@gmail.com>
2026-09-10 18:48:25 -0700

    Added relationships and fixed parameters

--- app/models/providers.py
+++ app/models/providers.py
@@ -1,8 +1,9 @@
-from app.database import Base
+from app.models.model_base import Base
 
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, ForeignKey, Boolean, text
 from sqlalchemy.dialects.mysql import SMALLINT
 from sqlalchemy.orm import mapped_column, relationship
+
 
 class Provider(Base):
     __tablename__ = 'providers'
@@ -24,8 +25,8 @@ class Provider(Base):
         nullable = False
     )
 
-    departmant_id = mapped_column(
-        SMALLINT(unsigned  =True),
+    department_id = mapped_column(
+        SMALLINT(unsigned = True),
         ForeignKey('departments.department_id'),
         nullable = False
     )
@@ -41,16 +42,38 @@ class Provider(Base):
 
     email = mapped_column(
         String(255),
-        nullable = False
+        nullable = False,
+        unique = True
     )
 
     active = mapped_column(
         Boolean,
-        nullable = False
+        nullable = False,
+        default = True,
+        server_default = text('TRUE')
     )
 
     department = relationship(
         'Department',
         back_populates = 'providers'
     )
-    
+
+    appointments = relationship(
+        'Appointment',
+        back_populates = 'providers'
+    )
+
+    encounters = relationship(
+        'Encounter',
+        back_populates = 'providers'
+    )
+
+    diagnosis_records = relationship(
+        'Diagnosis_Record',
+        back_populates = 'providers'
+    )
+
+    orders = relationship(
+        'Order',
+        back_populates = 'providers'
+    )
